dd12caa79f54f4e5af4d3cda62e7edbab9e6236a
Ilya Shevtsov <ishevtsov@icerockdev.com>
2022-07-08 11:32:57 +0700

    IRU-89 initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GitHubClientKMM"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/githubclientkmm/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.githubclientkmm
+
+expect class Platform() {
+    val platform: String
+}
