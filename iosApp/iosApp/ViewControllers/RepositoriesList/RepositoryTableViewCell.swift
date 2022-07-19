02105d760424b77d88d6822f1c171277987bb637
Ilya Shevtsov <ishevtsov@icerockdev.com>
2022-07-19 14:21:10 +0700

    IRU-89 removed highlighting row in list IOS

--- iosApp/iosApp/ViewControllers/RepositoriesList/RepositoryTableViewCell.swift
+++ iosApp/iosApp/ViewControllers/RepositoriesList/RepositoryTableViewCell.swift
@@ -19,7 +19,7 @@ class RepositoryTableViewCell: UITableViewCell {
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+        super.setSelected(false, animated: animated)
     }
     
     func setData(repo: Repo) {
